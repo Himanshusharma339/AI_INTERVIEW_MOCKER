ad40f57400d5b1324997275f75a2861a9e1c87fe
Himanshu Sharma <hs6592784@gmail.com>
2025-03-07 08:46:32 +0530

    Initial commit

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-        url: 'postgresql://ai%20mock%20interview_owner:aQ7GxFrR2qHj@ep-quiet-hat-a5sn0e5z.us-east-2.aws.neon.tech/ai-mock-interview?sslmode=require',
+        url: 'postgresql://neondb_owner:npg_WPxXjrw4vd1K@ep-old-bar-a8k17aoz-pooler.eastus2.azure.neon.tech/neondb?sslmode=require',
     }
 };
